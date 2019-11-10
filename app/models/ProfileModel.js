4649cd73ab53f7c9458a0b67c4ab1242747ff429
hendra sadewa <hendra.sadewa@hara.ag>
2019-11-10 21:20:23 +0700

    create profile model

---
+++ app/models/ProfileModel.js
@@ -0,0 +1,62 @@
+const mongoose = require('../configs/mongoose');
+
+const ProfileModel = mongoose.model(
+  'profiles',
+  new mongoose.Schema(
+    {
+      firstName: {
+        type: String,
+        required: true
+      },
+      lastName: {
+        type: String,
+        required: true
+      },
+      address: {
+        type: String,
+        required: true
+      },
+      dateOfBirth: {
+        type: Date,
+        required: true
+      },
+      placeOfBirth: {
+        type: String,
+        required: true
+      },
+      contacts: {
+        type: [
+          {
+            type: {
+              type: String,
+              enum: ['Phone Number', 'Email', 'Home Phone Number']
+            },
+            value: {
+              type: String
+            }
+          }
+        ]
+      },
+      relatedPerson: [
+        {
+          person: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'users'
+          },
+          relation: {
+            type: String,
+            enum: ['Parent', 'Children']
+          }
+        }
+      ]
+    },
+    {
+      timestamps: {
+        createdAt: 'created_at',
+        updatedAt: 'updated_at'
+      }
+    }
+  )
+);
+
+module.exports = ProfileModel;
