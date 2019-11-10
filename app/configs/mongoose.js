26276411ebfd1f4b21a244724e26472c923431fa
hendra sadewa <hendra.sadewa@hara.ag>
2019-11-10 21:19:36 +0700

    create mongoose instance configuration

---
+++ app/configs/mongoose.js
@@ -0,0 +1,48 @@
+require('dotenv').config();
+const mongoose = require('mongoose');
+
+// log connection events
+
+mongoose.Promise = Promise;
+
+mongoose.connection.on('connected', () => {
+  console.log('Connection Established');
+});
+
+mongoose.connection.on('reconnected', () => {
+  console.log('Connection Reestablished');
+});
+
+mongoose.connection.on('disconnected', () => {
+  console.log('Connection Disconnected');
+});
+
+mongoose.connection.on('close', () => {
+  console.log('Connection Closed');
+});
+
+mongoose.connection.on('error', error => {
+  console.log('ERROR: ' + error);
+});
+
+// connect to mongoose
+async function connect(url) {
+  try {
+    await mongoose.connect(url, {
+      useCreateIndex: true,
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      autoReconnect: true,
+      reconnectTries: 1000000,
+      reconnectInterval: 3000
+    });
+  } catch (error) {
+    console.log('ERROR: ' + error);
+  }
+}
+
+const { DATABASE_HOST, DATABASE_PORT, DATABASE_NAME } = process.env;
+
+connect(`mongodb://${DATABASE_HOST}:${DATABASE_PORT}/${DATABASE_NAME}`);
+
+module.exports = mongoose;
